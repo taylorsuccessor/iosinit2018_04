f9d36c5ba61fbcc7ecf9cbfe83accdfbddfe9d8e
Amjad Tubasi <eng.amjad.tubasi.at@gmail.com>
2018-04-07 14:43:23 +0300

    define APIManager and Util targets

---
+++ Ureed/APIManager/APIManager.h
@@ -0,0 +1,19 @@
+//
+//  APIManager.h
+//  APIManager
+//
+//  Created by Amjad Private on 4/7/18.
+//  Copyright © 2018 Ureed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIManager.
+FOUNDATION_EXPORT double APIManagerVersionNumber;
+
+//! Project version string for APIManager.
+FOUNDATION_EXPORT const unsigned char APIManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIManager/PublicHeader.h>
+
+
---
+++ Ureed/Util/Util.h
@@ -0,0 +1,19 @@
+//
+//  Util.h
+//  Util
+//
+//  Created by Amjad Private on 4/7/18.
+//  Copyright © 2018 Ureed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Util.
+FOUNDATION_EXPORT double UtilVersionNumber;
+
+//! Project version string for Util.
+FOUNDATION_EXPORT const unsigned char UtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Util/PublicHeader.h>
+
+
